042462858a1aa06956c97f38e13cd72e02dcb94e
Михаил Тучков <m2030110@gmail.com>
2025-07-03 18:59:05 +0300

    minor changes

--- src/leetCodeExecutor.ts
+++ src/leetCodeExecutor.ts
@@ -5,7 +5,7 @@ import * as cp from "child_process";
 import * as fse from "fs-extra";
 import * as os from "os";
 import * as path from "path";
-import * as requireFromString from "require-from-string";
+import requireFromString = require("require-from-string");
 import { ExtensionContext } from "vscode";
 import { ConfigurationChangeEvent, Disposable, MessageItem, window, workspace, WorkspaceConfiguration } from "vscode";
 import { Endpoint, IProblem, leetcodeHasInited, supportedPlugins } from "./shared";
@@ -262,7 +262,7 @@ class LeetCodeExecutor implements Disposable {
         }
     }
     
-    public async getDailyChallengeHistory(needTranslation?: boolean, days: number = 30): Promise<any[]> {
+    public async getDailyChallengeHistory(_needTranslation?: boolean, days: number = 30): Promise<any[]> {
         try {
             const https = require('https');
             
